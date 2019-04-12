3892f3004f0b7b5d4db6329f180eb1efc7ee36c5
Alex Petrakov <no_reply@example.com>
2019-04-12 15:02:58 +0300

    Add Feed filters bottom sheet.

--- app/src/main/java/com/github/varhastra/epicenter/main/feed/FeedFragment.kt
+++ app/src/main/java/com/github/varhastra/epicenter/main/feed/FeedFragment.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
+import butterknife.BindView
+import butterknife.ButterKnife
 
 import com.github.varhastra.epicenter.R
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 /**
  * A [Fragment] subclass that displays a list
@@ -15,12 +20,37 @@ import com.github.varhastra.epicenter.R
  */
 class FeedFragment : Fragment() {
 
+    @BindView(R.id.cg_feed_filters_magnitude)
+    lateinit var magnitudeChipGroup: ChipGroup
+
+    @BindView(R.id.cg_feed_filters_sort_by)
+    lateinit var sortingChipGroup: ChipGroup
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
+        val root = inflater.inflate(R.layout.fragment_feed, container, false)
+        ButterKnife.bind(this, root)
+
+        magnitudeChipGroup.setOnCheckedChangeListener { group, checkedId ->
+            group.children.forEach {
+                (it as? Chip)?.apply {
+                    isClickable = !isChecked
+                }
+            }
+        }
+
+        sortingChipGroup.setOnCheckedChangeListener { group, checkedId ->
+            group.children.forEach {
+                (it as? Chip)?.apply {
+                    isClickable = !isChecked
+                }
+            }
+        }
+
+        return root
     }
 
 

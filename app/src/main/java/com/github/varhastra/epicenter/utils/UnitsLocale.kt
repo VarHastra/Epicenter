fa89f2591df6bb583ea45c5d68791170772a0776
VarHastra <VarHastra@users.noreply.github.com>
2019-04-13 16:35:29 +0300

    Implement UnitsLocale.

---
+++ app/src/main/java/com/github/varhastra/epicenter/utils/UnitsLocale.kt
@@ -0,0 +1,40 @@
+package com.github.varhastra.epicenter.utils
+
+import java.util.*
+
+class UnitsLocale private constructor(val desc: String) {
+
+    companion object {
+        val METRIC = UnitsLocale("metric")
+        val IMPERIAL = UnitsLocale("imperial")
+
+        fun getDefault() = getFor(Locale.getDefault())
+
+        fun getFor(locale: Locale): UnitsLocale {
+            val countryCode = locale.country
+            return when (countryCode) {
+                in "US", "LR", "MM" -> IMPERIAL
+                else -> METRIC
+            }
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as UnitsLocale
+
+        if (desc != other.desc) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return desc.hashCode()
+    }
+
+    override fun toString(): String {
+        return "UnitsLocale(desc='$desc')"
+    }
+}

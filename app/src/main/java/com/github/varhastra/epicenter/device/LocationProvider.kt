041c32426f2e6e7d160ab1451058f3d52cdfcc4d
VarHastra <VarHastra@users.noreply.github.com>
2019-05-08 21:51:04 +0300

    Add geocoding feature.

--- app/src/main/java/com/github/varhastra/epicenter/device/LocationProvider.kt
+++ app/src/main/java/com/github/varhastra/epicenter/device/LocationProvider.kt
@@ -1,19 +1,20 @@
 package com.github.varhastra.epicenter.device
 
 import android.content.Context
+import android.location.Geocoder
 import com.github.varhastra.epicenter.App
 import com.github.varhastra.epicenter.domain.DataSourceCallback
 import com.github.varhastra.epicenter.domain.LocationDataSource
 import com.github.varhastra.epicenter.domain.model.Coordinates
 import com.github.varhastra.epicenter.domain.model.Position
 import com.google.android.gms.location.LocationServices
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.debug
-import org.jetbrains.anko.warn
+import org.jetbrains.anko.*
+import java.lang.Exception
 
 class LocationProvider(val context: Context = App.instance) : LocationDataSource {
 
     private val locationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    private val geocoder = Geocoder(context)
     private val logger = AnkoLogger(this.javaClass)
 
     override fun getLastLocation(callback: DataSourceCallback<Position>) {
@@ -33,5 +34,28 @@ class LocationProvider(val context: Context = App.instance) : LocationDataSource
         } catch (e: SecurityException) {
             callback.onFailure(e)
         }
+    }
+
+    override fun getLocationName(coordinates: Coordinates, callback: DataSourceCallback<String>) {
+        doAsync(callback::onFailure) {
+            val addresses = geocoder.getFromLocation(coordinates.latitude, coordinates.longitude, 1)
+            uiThread {
+                if (addresses.isEmpty()) {
+                    logger.warn("getLocationName() geocoder has returned empty list")
+                    callback.onFailure(null)
+                } else {
+                    val address = addresses[0]
+                    logger.debug("getLocationName() $address")
+                    val addressString = address.locality?.plus(", ").orEmpty() +
+                            address.adminArea?.plus(", ").orEmpty() +
+                            address.countryCode.orEmpty()
+                    callback.onResult(addressString)
+                }
+            }
+        }
+    }
+
+    override fun isGeoCodingAvailable(): Boolean {
+        return Geocoder.isPresent()
     }
 }
--- app/src/main/java/com/github/varhastra/epicenter/domain/LocationDataSource.kt
+++ app/src/main/java/com/github/varhastra/epicenter/domain/LocationDataSource.kt
@@ -6,4 +6,8 @@ import com.github.varhastra.epicenter.domain.model.Position
 interface LocationDataSource {
 
     fun getLastLocation(callback: DataSourceCallback<Position>)
+
+    fun getLocationName(coordinates: Coordinates, callback: DataSourceCallback<String>)
+
+    fun isGeoCodingAvailable(): Boolean
 }

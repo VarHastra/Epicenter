5a480bf3cfd682cfe5e0aba788a4ce7f49b13a97
VarHastra <VarHastra@users.noreply.github.com>
2019-05-12 14:19:25 +0300

    Implement equals() hashCode() and toString() for RemoteEvent.

--- app/src/main/java/com/github/varhastra/epicenter/domain/model/RemoteEvent.kt
+++ app/src/main/java/com/github/varhastra/epicenter/domain/model/RemoteEvent.kt
@@ -10,6 +10,30 @@ class RemoteEvent(val event: Event, point: Coordinates? = null) {
         }
     }
 
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as RemoteEvent
+
+        if (event != other.event) return false
+        if (distance != other.distance) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = event.hashCode()
+        result = 31 * result + (distance?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        return "RemoteEvent(event=$event, distance=$distance)"
+    }
+
+
     companion object {
         fun from(event: Event, point: Coordinates? = null) = RemoteEvent(event, point)
 

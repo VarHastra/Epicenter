05254911b3ba006f99ed0caeabc2ce3998a61a14
Alex Petrakov <no_reply@example.com>
2019-05-26 22:13:34 +0300

    Migrate MockProvider to moshi.

--- app/src/main/java/com/github/varhastra/epicenter/data/networking/usgs/UsgsMockProvider.kt
+++ app/src/main/java/com/github/varhastra/epicenter/data/networking/usgs/UsgsMockProvider.kt
@@ -6,17 +6,21 @@ import com.github.varhastra.epicenter.App
 import com.github.varhastra.epicenter.data.networking.EventServiceProvider
 import com.github.varhastra.epicenter.data.networking.Network
 import com.github.varhastra.epicenter.data.networking.usgs.model.UsgsResponse
-import com.google.gson.Gson
+import com.squareup.moshi.Moshi
+import okio.Okio
 import java.util.concurrent.Executors
 
-class UsgsMockProvider(val context: Context = App.instance, val gson: Gson = Network.gson) : EventServiceProvider {
+class UsgsMockProvider(val context: Context = App.instance, val moshi: Moshi = Network.moshi) : EventServiceProvider {
     override fun getWeekFeed(responseCallback: EventServiceProvider.ResponseCallback) {
         val handler = Handler()
         Executors.newSingleThreadExecutor().execute {
-            val reader = context.assets.open("mock_response.json").bufferedReader()
-            val response = gson.fromJson<UsgsResponse>(reader, UsgsResponse::class.java)
+            val inputStream = context.assets.open("mock_response.json")
+            val reader = Okio.buffer(Okio.source(inputStream))
+            val adapter = moshi.adapter<UsgsResponse>(UsgsResponse::class.java)
+            val response = adapter.fromJson(reader)
+
             handler.post {
-                responseCallback.onResult(response)
+                response?.let { responseCallback.onResult(response) }
             }
         }
 

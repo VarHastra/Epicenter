e474c1677496ad3eeea8544d479f2c7945be405b
VarHastra <VarHastra@users.noreply.github.com>
2020-01-11 20:34:34 +0300

    Add suspending functions for fetching events to EventsRepository

--- app/src/main/java/com/github/varhastra/epicenter/data/EventsDataSource.kt
+++ app/src/main/java/com/github/varhastra/epicenter/data/EventsDataSource.kt
@@ -1,5 +1,7 @@
 package com.github.varhastra.epicenter.data
 
+import com.github.varhastra.epicenter.common.functionaltypes.Either
+import com.github.varhastra.epicenter.common.functionaltypes.ifSuccess
 import com.github.varhastra.epicenter.data.network.EventServiceProvider
 import com.github.varhastra.epicenter.data.network.EventServiceResponse
 import com.github.varhastra.epicenter.data.network.usgs.UsgsServiceProvider
@@ -8,6 +10,7 @@ import com.github.varhastra.epicenter.domain.repos.EventsRepository
 import com.github.varhastra.epicenter.domain.repos.RepositoryCallback
 import org.jetbrains.anko.AnkoLogger
 import org.threeten.bp.Instant
+import java.util.*
 
 class EventsDataSource private constructor(
         private val serviceProvider: EventServiceProvider
@@ -47,6 +50,17 @@ class EventsDataSource private constructor(
         })
     }
 
+    override suspend fun getWeekFeedSuspending(forceLoad: Boolean): Either<List<Event>, Throwable> {
+        if (!forceLoad && eventsFeedCache.isNotEmpty()) {
+            val list = eventsFeedCache.values.toList()
+            return Either.Success(list)
+        }
+
+        return serviceProvider.getWeekFeedSuspending()
+                .map { response -> response.mapToModel() }
+                .ifSuccess { updateFeedCache(it) }
+    }
+
     override fun getEvent(eventId: String, callback: RepositoryCallback<Event>) {
         val event = eventsFeedCache[eventId]
 
@@ -54,6 +68,16 @@ class EventsDataSource private constructor(
             callback.onResult(event)
         } else {
             callback.onFailure(IllegalStateException("Events cache doesn't contain event with the given id $eventId."))
+        }
+    }
+
+    override suspend fun getEventSuspending(eventId: UUID): Either<Event, Throwable> {
+        val cachedEvent = eventsFeedCache[eventId.toString()]
+
+        return if (cachedEvent != null) {
+            Either.Success(cachedEvent)
+        } else {
+            Either.Failure(IllegalStateException("Unable to find an event with the given id: $eventId."))
         }
     }
 
--- app/src/main/java/com/github/varhastra/epicenter/domain/repos/EventsRepository.kt
+++ app/src/main/java/com/github/varhastra/epicenter/domain/repos/EventsRepository.kt
@@ -1,7 +1,9 @@
 package com.github.varhastra.epicenter.domain.repos
 
+import com.github.varhastra.epicenter.common.functionaltypes.Either
 import com.github.varhastra.epicenter.domain.model.Event
 import org.threeten.bp.Instant
+import java.util.*
 
 interface EventsRepository {
 
@@ -10,7 +12,11 @@ interface EventsRepository {
             forceLoad: Boolean = false
     )
 
+    suspend fun getWeekFeedSuspending(forceLoad: Boolean = false): Either<List<Event>, Throwable>
+
     fun getEvent(eventId: String, callback: RepositoryCallback<Event>)
+
+    suspend fun getEventSuspending(eventId: UUID): Either<Event, Throwable>
 
     fun getWeekFeedLastUpdated(): Instant
 

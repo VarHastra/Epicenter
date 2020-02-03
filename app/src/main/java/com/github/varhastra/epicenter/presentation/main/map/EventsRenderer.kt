5303834d5ba6e2895ccbfc5af7e45532d7b87f1b
VarHastra <VarHastra@users.noreply.github.com>
2020-02-03 20:23:22 +0300

    Clean up EventsRenderer

--- app/src/main/java/com/github/varhastra/epicenter/presentation/main/map/EventsRenderer.kt
+++ app/src/main/java/com/github/varhastra/epicenter/presentation/main/map/EventsRenderer.kt
@@ -1,11 +1,11 @@
 package com.github.varhastra.epicenter.presentation.main.map
 
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.graphics.drawable.LayerDrawable
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
 import android.view.ViewGroup
+import androidx.annotation.ColorRes
 import com.github.varhastra.epicenter.R
 import com.github.varhastra.epicenter.common.extensions.getColorCompat
 import com.github.varhastra.epicenter.presentation.common.AlertLevel
@@ -27,30 +27,19 @@ class EventsRenderer(
         clusterManager: ClusterManager<EventMarker>
 ) : DefaultClusterRenderer<EventMarker>(context, map, clusterManager) {
 
-    // Unfortunately some members of DefaultClusterRenderer are private, including mIconGenerator, mDensity, mColoredCircleBackground.
-    // We want to render our own cluster icons.
-    // It is possible to create our own full implementation of DefaultClusterRenderer, however it
-    // internally relies on some private functions in other classes. So the quickest and the most
-    // straightforward solution here is to just create our own IconGenerator, and other things that copy
-    // the behavior of their counterparts in the DefaultClusterRenderer.
-    private val density = context.resources.displayMetrics.density
+    private val clusterBgDrawable: ShapeDrawable = ShapeDrawable(OvalShape())
 
-    private lateinit var coloredCircleBackground: ShapeDrawable
-    private lateinit var outline: ShapeDrawable
-
-    private val iconGenerator = IconGenerator(context)
-
-    /**
-     * Cluster icons cache. It distinguishes icons by their bucket and alert level.
-     */
-    private val icons = mutableMapOf<Pair<Int, AlertLevel?>, BitmapDescriptor>()
-
-
-    init {
-        iconGenerator.setContentView(makeSquareTextView(context))
-        iconGenerator.setTextAppearance(R.style.ClusterIconTextAppearance)
-        iconGenerator.setBackground(makeClusterBackground())
+    private val clusterOutlineDrawable: ShapeDrawable = ShapeDrawable(OvalShape()).apply {
+        alpha = 128
     }
+
+    private val iconGenerator = IconGenerator(context).apply {
+        setContentView(makeSquareTextView())
+        setTextAppearance(R.style.ClusterIconTextAppearance)
+        setBackground(makeClusterBackground())
+    }
+
+    private val icons = mutableMapOf<Pair<Int, AlertLevel>, BitmapDescriptor>()
 
 
     override fun onBeforeClusterItemRendered(item: EventMarker, markerOptions: MarkerOptions) {
@@ -64,41 +53,48 @@ class EventsRenderer(
     }
 
     override fun onBeforeClusterRendered(cluster: Cluster<EventMarker>, markerOptions: MarkerOptions) {
-        val maxAlertLevel = cluster.items.map { it.alertLevel }.maxBy { it.value }
-                ?: AlertLevel.LEVEL_0
+        val alertLevel = findHighestAlertLevelIn(cluster)
         val bucket = getBucket(cluster)
 
-        var descriptor: BitmapDescriptor? = icons[bucket to maxAlertLevel]
-        if (descriptor == null) {
-            val color = context.getColorCompat(maxAlertLevel.colorResId)
-            coloredCircleBackground.paint.color = color
-            outline.paint.color = color
-            descriptor = BitmapDescriptorFactory.fromBitmap(iconGenerator.makeIcon(getClusterText(bucket)))
-            icons[bucket to maxAlertLevel] = descriptor
+        val descriptor = icons.getOrPut(bucket to alertLevel) {
+            setClusterDrawablesColor(alertLevel.colorResId)
+            BitmapDescriptorFactory.fromBitmap(iconGenerator.makeIcon(getClusterText(bucket)))
         }
 
-        markerOptions.icon(descriptor)
-        markerOptions.zIndex(12.0f)
+        markerOptions.apply {
+            icon(descriptor)
+            zIndex(12.0f)
+        }
     }
 
-    private fun makeSquareTextView(context: Context): SquareTextView {
-        val squareTextView = SquareTextView(context)
-        val layoutParams =
+    private fun findHighestAlertLevelIn(cluster: Cluster<EventMarker>): AlertLevel {
+        return cluster.items.map { it.alertLevel }.maxBy { it.value }
+                ?: throw IllegalStateException("Empty cluster: $cluster.")
+    }
+
+    private fun setClusterDrawablesColor(@ColorRes resId: Int) {
+        val color = context.getColorCompat(resId)
+        clusterBgDrawable.paint.color = color
+        clusterOutlineDrawable.paint.color = color
+    }
+
+    private fun makeSquareTextView(): SquareTextView {
+        val density = context.resources.displayMetrics.density
+        val params =
                 ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        squareTextView.layoutParams = layoutParams
-        squareTextView.id = com.google.maps.android.R.id.amu_text
         val twelveDpi = (12 * density).toInt()
-        squareTextView.setPadding(twelveDpi, twelveDpi, twelveDpi, twelveDpi)
-        return squareTextView
+        return SquareTextView(context).apply {
+            layoutParams = params
+            id = com.google.maps.android.R.id.amu_text
+            setPadding(twelveDpi, twelveDpi, twelveDpi, twelveDpi)
+        }
     }
 
     private fun makeClusterBackground(): LayerDrawable {
-        coloredCircleBackground = ShapeDrawable(OvalShape())
-        outline = ShapeDrawable(OvalShape())
-        outline.alpha = 128
-        val background = LayerDrawable(arrayOf<Drawable>(outline, coloredCircleBackground))
+        val density = context.resources.displayMetrics.density
         val strokeWidth = (density * 3).toInt()
-        background.setLayerInset(1, strokeWidth, strokeWidth, strokeWidth, strokeWidth)
-        return background
+        return LayerDrawable(arrayOf(clusterOutlineDrawable, clusterBgDrawable)).apply {
+            setLayerInset(1, strokeWidth, strokeWidth, strokeWidth, strokeWidth)
+        }
     }
 }

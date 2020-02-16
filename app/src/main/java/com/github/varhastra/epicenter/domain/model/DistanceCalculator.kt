c9c948755539d2801869f4c788a18dda35dfd96c
VarHastra <VarHastra@users.noreply.github.com>
2020-02-16 21:33:08 +0300

    Replace java.lang.Math with kotlin.math within DistanceCalculator

--- app/src/main/java/com/github/varhastra/epicenter/domain/model/DistanceCalculator.kt
+++ app/src/main/java/com/github/varhastra/epicenter/domain/model/DistanceCalculator.kt
@@ -1,7 +1,9 @@
 package com.github.varhastra.epicenter.domain.model
 
 import kotlin.math.atan2
+import kotlin.math.cos
 import kotlin.math.sin
+import kotlin.math.sqrt
 
 
 private const val EARTH_RADIUS_KM = 6371
@@ -14,10 +16,9 @@ private const val EARTH_RADIUS_KM = 6371
 fun haversineDistance(point1: Coordinates, point2: Coordinates): Double {
     val dLat = degToRad(point2.latitude - point1.latitude)
     val dLon = degToRad(point2.longitude - point1.longitude)
-    val a =
-        sin(dLat / 2) * sin(dLat / 2) +
-                Math.cos(degToRad(point1.latitude)) * Math.cos(degToRad(point2.latitude)) *
-                Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    val a = sin(dLat / 2) * sin(dLat / 2) +
+            cos(degToRad(point1.latitude)) * cos(degToRad(point2.latitude)) *
+            sin(dLon / 2) * sin(dLon / 2)
 
-    return 2 * atan2(Math.sqrt(a), Math.sqrt(1 - a)) * EARTH_RADIUS_KM
+    return 2 * atan2(sqrt(a), sqrt(1 - a)) * EARTH_RADIUS_KM
 }

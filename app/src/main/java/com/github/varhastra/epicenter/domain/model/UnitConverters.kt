4791f697399e574db85e4d0626a3207be5687b21
VarHastra <VarHastra@users.noreply.github.com>
2020-02-16 22:23:54 +0300

    Add checks against negative distance arguments in UnitConverter

--- app/src/main/java/com/github/varhastra/epicenter/domain/model/UnitConverters.kt
+++ app/src/main/java/com/github/varhastra/epicenter/domain/model/UnitConverters.kt
@@ -7,13 +7,25 @@ private const val MI_TO_KM_RATIO = 1.609
 private const val KM_TO_M_RATIO = 1000
 private const val LAT_DEG_TO_KM_RATIO = 111.1
 
-fun kmToMi(km: Double) = km / MI_TO_KM_RATIO
+fun kmToMi(km: Double): Double {
+    if (km < 0) throw IllegalArgumentException("Negative arguments are not allowed. The argument was $km.")
+    return km / MI_TO_KM_RATIO
+}
 
-fun kmToM(km: Double) = km * KM_TO_M_RATIO
+fun kmToM(km: Double): Double {
+    if (km < 0) throw IllegalArgumentException("Negative arguments are not allowed. The argument was $km.")
+    return km * KM_TO_M_RATIO
+}
 
-fun miToKm(mi: Double) = mi * MI_TO_KM_RATIO
+fun miToKm(mi: Double): Double {
+    if (mi < 0) throw IllegalArgumentException("Negative arguments are not allowed. The argument was $mi.")
+    return mi * MI_TO_KM_RATIO
+}
 
-fun mToKm(m: Double) = m / KM_TO_M_RATIO
+fun mToKm(m: Double): Double {
+    if (m < 0) throw IllegalArgumentException("Negative arguments are not allowed. The argument was $m.")
+    return m / KM_TO_M_RATIO
+}
 
 fun latDegToKm(latDegree: Double) = latDegree * LAT_DEG_TO_KM_RATIO
 

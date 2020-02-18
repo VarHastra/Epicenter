051d01d27197f3f1e304e2e89bc3ece1d4983dc4
VarHastra <VarHastra@users.noreply.github.com>
2020-02-18 18:25:51 +0300

    Remove unused properties of UsgsResponse

--- app/src/main/java/com/github/varhastra/epicenter/data/network/usgs/model/UsgsRepsonses.kt
+++ app/src/main/java/com/github/varhastra/epicenter/data/network/usgs/model/UsgsRepsonses.kt
@@ -111,12 +111,12 @@ data class Properties(
          * Time when the event was most recently updated.
          * Times are reported in milliseconds since the epoch.
          */
-        val updated: Long? = null,
+//        val updated: Long? = null,
 
         /**
          * Timezone offset from UTC in minutes at the event epicenter.
          */
-        val tz: Int? = null,
+//        val tz: Int? = null,
 
         /**
          * Link to USGS Event Page for event.
@@ -126,7 +126,7 @@ data class Properties(
         /**
          * Link to GeoJSON detail feed from a GeoJSON summary feed.
          */
-        val detail: String? = null,
+//        val detail: String? = null,
 
         /**
          * The total number of felt reports submitted to the DYFI? system.
@@ -139,7 +139,7 @@ data class Properties(
          * for the purposes of this API, intensity is expected as the decimal
          * equivalent of the roman numeral.
          */
-        val cdi: Double? = null,
+//        val cdi: Double? = null,
 
         /**
          * The maximum estimated instrumental intensity for the event.
@@ -147,13 +147,13 @@ data class Properties(
          * for the purposes of this API, intensity is expected as the decimal equivalent
          * of the roman numeral. Learn more about magnitude vs. intensity.
          */
-        val mmi: Double? = null,
+//        val mmi: Double? = null,
 
         /**
          * The alert level from the PAGER earthquake impact scale.
          * Typical Values: "green", "yellow", "red".
          */
-        val alert: String? = null,
+//        val alert: String? = null,
 
         /**
          * This flag is set to "1" for large events in oceanic regions and "0" otherwise.
@@ -164,72 +164,72 @@ data class Properties(
          * A number describing how significant the event is. Larger numbers indicate a more significant event.
          * Values: [0, 1000]
          */
-        val sig: Int? = null,
+//        val sig: Int? = null,
 
         /**
          * The ID of a data contributor.
          */
-        val net: String? = null,
+//        val net: String? = null,
 
         /**
          * An identifying code assigned by - and unique from - the corresponding source for the event.
          */
-        val code: String? = null,
+//        val code: String? = null,
 
         /**
          * A comma-separated list of event ids that are associated to an event.
          * Value: ",ci15296281,us2013mqbd,at00mji9pf,"
          */
-        val ids: String? = null,
+//        val ids: String? = null,
 
         /**
          * A comma-separated list of network contributors.
          * Value: ",us,nc,ci,"
          */
-        val sources: String? = null,
+//        val sources: String? = null,
 
         /**
          * A comma-separated list of product types associated to this event.
          * Value: “,cap,dyfi,general-link,origin,p-wave-travel-times,phase-data,”
          */
-        val types: String? = null,
+//        val types: String? = null,
 
         /**
          * The total number of seismic stations used to determine earthquake location.
          */
-        val nst: Int? = null,
+//        val nst: Int? = null,
 
         /**
          * Horizontal distance from the epicenter to the nearest station (in degrees). 1 degree is approximately 111.2 kilometers.
          */
-        val dmin: Double? = null,
+//        val dmin: Double? = null,
 
         /**
          * The root-mean-square (RMS) travel time residual, in sec, using all weights.
          * This parameter provides a measure of the fit of the observed arrival times to the predicted arrival times for this location. Smaller numbers reflect a better fit of the data.
          */
-        val rms: Double? = null,
+//        val rms: Double? = null,
 
         /**
          * The largest azimuthal gap between azimuthally adjacent stations (in degrees).
          * In general, the smaller this number,
          * the more reliable is the calculated horizontal position of the earthquake.
          */
-        val gap: Double? = null,
+//        val gap: Double? = null,
 
         /**
          * The method or algorithm used to calculate the preferred magnitude for the event.
          */
-        val magType: String? = null,
+        val magType: String? = null
 
         /**
          * Type of seismic event.
          * Values: “earthquake”, “quarry”
          */
-        val type: String? = null,
+//        val type: String? = null,
 
         /**
          * Magnitude + place.
          */
-        val title: String? = null
+//        val title: String? = null
 )

348bc196181f07d139a48fd2e8dc55700e56bd48
VarHastra <VarHastra@users.noreply.github.com>
2020-05-03 19:25:02 +0300

    Override equals(), hashCode() and toString() in Destination

--- app/src/main/java/me/alex/pet/apps/epicenter/presentation/common/navigation/Destination.kt
+++ app/src/main/java/me/alex/pet/apps/epicenter/presentation/common/navigation/Destination.kt
@@ -6,4 +6,26 @@ abstract class Destination {
     abstract val fragment: Fragment
 
     open val tag: String = this::class.java.simpleName
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Destination
+
+        if (fragment != other.fragment) return false
+        if (tag != other.tag) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = fragment.hashCode()
+        result = 31 * result + tag.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Destination(fragment=$fragment, tag='$tag')"
+    }
 }
